c95514d779d48a911bc6fdc2cce165015f86de52
nxcc <73293562+nxcc@users.noreply.github.com>
2023-02-03 12:02:09 +0100

    add test of examples to release workflow (#4)
    Co-authored-by: nxcc <>

--- scripts/test-all-examples.sh
+++ scripts/test-all-examples.sh
@@ -3,7 +3,17 @@
 set -euo pipefail
 cd "$(dirname "$0")/../examples"
 
+# setup
+
+export SOPS_AGE_KEY=AGE-SECRET-KEY-14QUHLE5A6UNSKNYXLF5ZA26P3NCFX8P68JQ066T7VJ6JW5G8FHWQN4HAUQ
+
+if [[ ! -f control-repository/control/demo.cue ]]; then
+    cp control-repository/control/demo.cue.template control-repository/control/demo.cue
+fi
+
 trap "echo '  FAILED'" EXIT
+
+# run tests
 
 echo configmap...
 cuegen configmap | grep -q "2023-01-10T12:00:00Z"
@@ -14,8 +24,7 @@ cuegen values | grep -q "7 replicas configured"
 echo "  OK"
 
 echo encrypted...
-SOPS_AGE_KEY=AGE-SECRET-KEY-14QUHLE5A6UNSKNYXLF5ZA26P3NCFX8P68JQ066T7VJ6JW5G8FHWQN4HAUQ \
-    cuegen encrypted | grep -q IEtFWS0tLS0tCk1JSUV2Z0paTXF
+cuegen encrypted | grep -q IEtFWS0tLS0tCk1JSUV2Z0paTXF
 echo "  OK"
 
 echo control-repository
@@ -23,6 +32,8 @@ cuegen control-repository/control/dev-cluster/wekan-dev/ | grep -q "namespace: c
 cuegen control-repository/control/prod-cluster/wekan-prod/ | grep -q "namespace: cuegen-demo-prod"
 cuegen control-repository/control/prod-cluster/wekan-qa/ | grep -q "namespace: cuegen-demo-qa"
 echo "  OK"
+
+# done
 
 trap "" EXIT
 echo "all tests successful"
